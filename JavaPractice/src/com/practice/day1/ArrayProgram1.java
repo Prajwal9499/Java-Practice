2bf6516df3f6b83497c7a3c5c29291f68d422880
prajwal <prajwal9499@gmail.com>
2025-05-30 15:40:02 +0530

    Hello

--- JavaPractice/src/com/practice/day1/ArrayProgram1.java
+++ JavaPractice/src/com/practice/day1/ArrayProgram1.java
@@ -4,7 +4,7 @@ public class ArrayProgram1 {
 
 	public static void main(String[] args) {
 		
-		String my="ajay";
+		String my="vijay";
 		String a[][]= {{"Prajwal","Pramos","ajay","likith"},{"Umesh","veeresh","Srinivas","Adarsh"}};
 		
 		for(int i=0;i<a.length;i++)
